c593604d36279955d7ca812c5aff0fe432d2217e
Maxim Radugin <maxim.radugin@gmail.com>
2019-11-07 15:10:26 +0200

    Added 1MB read and write buffers, code cleanup

--- dmcryptfile.c
+++ dmcryptfile.c
@@ -52,12 +52,13 @@ THE SOFTWARE.
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <limits.h>
 
 #include "crypto_backend.h"
 
-#define SECTOR_SIZE 512
+static const size_t SECTOR_SIZE = 512;
 
-#define FILENAME_LEN 255
+#define CIPHER_NAME_MAX 21
 
 enum {
     ACTION_NONE,
@@ -67,14 +68,14 @@ enum {
 
 static struct option long_options[] =
 {
-    {"cipher",    required_argument, 0, 'c'},
+    {"cipher",      required_argument, 0, 'c'},
     {"key-size",    required_argument, 0, 's'},
     {"key-file",    required_argument, 0, 'f'},
-    {"in-file",    required_argument, 0, 'i'},
+    {"in-file",     required_argument, 0, 'i'},
     {"out-file",    required_argument, 0, 'o'},
-    {"enc", no_argument, 0, 'e'},
-    {"dec", no_argument, 0, 'd'},
-    {"help", no_argument, 0, 'h'},
+    {"enc",         no_argument,       0, 'e'},
+    {"dec",         no_argument,       0, 'd'},
+    {"help",        no_argument,       0, 'h'},
     {0, 0, 0, 0}
 };
 
@@ -93,7 +94,7 @@ void display_usage(char* name) {
     printf("processing is performed. It is up to the user to provide strong key.\n");
     printf("For available ciphers, chain modes and key sizes check /proc/crypto.\n");
     printf("Only plain IV mode is supported.\n");
-    printf("Input file size must be multiple of sector size i.e. %d bytes.\n", SECTOR_SIZE);
+    printf("Input file size must be multiple of sector size i.e. %lu bytes.\n", SECTOR_SIZE);
     printf("If input file is not multiple of sector size, output will be truncated.\n\n");
     printf("Mandatory arguments:\n");
     printf(" --cipher,-c   Cipher-chainmode-ivmode, for example, aes-xts-plain\n");
@@ -108,29 +109,50 @@ void display_usage(char* name) {
 
 }
 
-int main(int argc, char * const argv[]) {
-    int option_index = 0;
-    int c;
+char* load_key(const char* keyfile, int keysize)
+{
+    char* key = NULL;
+    int key_fd = open(keyfile, O_RDONLY);
+    if (key_fd < 0) {
+        perror("Unable to open key file");
+    }
+    else {
+        key = (char*)malloc(keysize);
+        if (read(key_fd, key, keysize) != keysize) {
+            fprintf(stderr, "Key file size should be at exactly %d bytes\n", keysize);
+            free(key);
+            key = NULL;
+        }
+        else {
+            printf("Key loaded from %s, %d bits\n", (char*)&keyfile, keysize*8);
+        }
+        close(key_fd);
+    }
+    return key;
+}
 
+int main(int argc, char * const argv[]) {
     int action = ACTION_NONE;
-    char infile[FILENAME_LEN] = {0};
-    char outfile[FILENAME_LEN] = {0};
+    char infile[PATH_MAX] = {0};
+    char outfile[PATH_MAX] = {0};
 
     int in_fd = -1;
     int out_fd = -1;
 
-    char keyfile[FILENAME_LEN] = {0};
-    char *key = 0;
+    char keyfile[PATH_MAX] = {0};
+    char *key = NULL;
     int keysize = 0;
 
     int error = 0;
 
-    char ciphername[21] = {0};
-    char chainmode[21] = {0};
-    char ivmode[21] = {0};
+    char ciphername[CIPHER_NAME_MAX] = {0};
+    char chainmode[CIPHER_NAME_MAX] = {0};
+    char ivmode[CIPHER_NAME_MAX] = {0};
 
-    struct crypt_cipher* cipher = 0;
+    struct crypt_cipher* cipher = NULL;
 
+    int option_index = 0;
+    int c;
     while ((c = getopt_long (argc, argv, "eds:f:c:i:o:h?",
             long_options, &option_index)) != -1) {
         switch (c) {
@@ -144,26 +166,27 @@ int main(int argc, char * const argv[]) {
                 break;
             case 'i':
                 if (optarg) {
-                    snprintf((char*)&infile, FILENAME_LEN, "%s", optarg);
+                    snprintf(infile, PATH_MAX, "%s", optarg);
                 }
                 break;
             case 'o':
                 if (optarg) {
-                    snprintf((char*)&outfile, FILENAME_LEN, "%s", optarg);
+                    snprintf(outfile, PATH_MAX, "%s", optarg);
                 }
                 break;
             case 'f':
                 if (optarg) {
-                    snprintf((char*)&keyfile, FILENAME_LEN, "%s", optarg);
+                    snprintf(keyfile, PATH_MAX, "%s", optarg);
                 }
                 break;
             case 's':
-                if (optarg)
+                if (optarg) {
                     sscanf(optarg, "%i", &keysize);
+                }
                 break;
             case 'c':
                 if (optarg) {
-                    if (sscanf(optarg, "%20[^-]-%20[^-]-%20s", (char*)&ciphername, (char*)&chainmode, (char*)&ivmode) != 3) {
+                    if (sscanf(optarg, "%20[^-]-%20[^-]-%20s", ciphername, chainmode, ivmode) != 3) {
                         fprintf(stderr, "Invalid cipher format specified %s, expected format cipher-chainmode-ivmode\n", optarg);
                         error = 1;
                     }
@@ -186,50 +209,53 @@ int main(int argc, char * const argv[]) {
         }
     }
 
-    if (!strlen((const char *)&keyfile) || !strlen((const char *)&infile) || !strlen((const char *)&outfile)) {
-        error = 1;
+    if (action == ACTION_NONE)
+    {
+        fprintf(stderr, "Action - encrypt/decrypt not specified\n");
+        return 1;
     }
 
-    if (!error) {
-        int key_fd = open((const char*)&keyfile, O_RDONLY);
-        if (key_fd < 0) {
-            perror("Unable to open key file");
-        }
-        else {
-            key = (char*)malloc(keysize);
-            if (read(key_fd, key, keysize) != keysize) {
-                fprintf(stderr, "Key file size should be at least %d bytes\n", keysize);
-                error = 1;
-                free(key);
-                key = 0;
-            }
-            else {
-                printf("Key loaded from %s, %d bits\n", (char*)&keyfile, keysize*8);
-            }
-        }
+    if (!strlen(keyfile))
+    {
+        fprintf(stderr, "Key file not specified\n");
+        return 1;
+    }
+    if (!strlen(infile))
+    {
+        fprintf(stderr, "Input file not specified\n");
+        return 1;
+    }
+    if (!strlen(outfile))
+    {
+        fprintf(stderr, "Output file not specified\n");
+        return 1;
     }
 
-    if (!error) {
-        if (strcmp((const char*)&infile, (const char*)&outfile) != 0) {
-            in_fd = open((const char*)&infile, O_RDONLY);
-            if (in_fd < 0) {
-                perror("Unable to open input file");
-                error = 1;
-            }
-            out_fd = open((const char*)&outfile, O_CREAT | O_TRUNC| O_RDWR, (S_IRUSR|S_IWUSR|S_IRGRP|S_IWGRP));
-            if (out_fd < 0) {
-                perror("Unable to create output file");
-                error = 1;
-            }
+    key = load_key(keyfile, keysize);
+    if (!key)
+    {
+        return 1;
+    }
+
+    if (strcmp((const char*)&infile, (const char*)&outfile) != 0) {
+        in_fd = open((const char*)&infile, O_RDONLY);
+        if (in_fd < 0) {
+            perror("Unable to open input file");
+            error = 1;
         }
-        else {
-            in_fd = open((const char*)&infile, O_RDWR);
-            if (in_fd < 0) {
-                perror("Unable to open input file");
-                error = 1;
-            }
-            out_fd = in_fd;
+        out_fd = open((const char*)&outfile, O_CREAT | O_TRUNC | O_RDWR, (S_IRUSR|S_IWUSR|S_IRGRP|S_IWGRP));
+        if (out_fd < 0) {
+            perror("Unable to create output file");
+            error = 1;
         }
+    }
+    else {
+        in_fd = open((const char*)&infile, O_RDWR);
+        if (in_fd < 0) {
+            perror("Unable to open input file");
+            error = 1;
+        }
+        out_fd = in_fd;
     }
 
     if (!error) {
@@ -241,20 +267,26 @@ int main(int argc, char * const argv[]) {
     }
 
     if (!error) {
-        char sector[SECTOR_SIZE];
-        char out_sector[SECTOR_SIZE];
-        char iv[16];
-        int r = 0;
-        unsigned int scnt = 0;
-        memset((void*)&iv, 0, sizeof(iv));
+        const size_t BUFFER_SECTOR_COUNT = 2048;
+        const size_t BUFFER_SIZE = SECTOR_SIZE * BUFFER_SECTOR_COUNT;
+        char read_buffer[BUFFER_SIZE];
+        char write_buffer[BUFFER_SIZE];
+        char iv[16] = {};
+        int bytes_read = 0;
+        size_t scnt = 0;
         printf("Processing...\n");
-        while ((r = read(in_fd, (char*)&sector, SECTOR_SIZE)) == SECTOR_SIZE) {
-            *(unsigned int *)iv = scnt & 0xffffffff;
+        while (!error && (bytes_read = read(in_fd, (char*)&read_buffer, BUFFER_SIZE)) > 0) {
+            if (bytes_read % SECTOR_SIZE != 0)
+            {
+                perror("Read size is not multiple of sector size");
+                error = 5;
+                break;
+            }
             // Same file for output
             if (in_fd == out_fd) {
                 off64_t pos = lseek64(in_fd, 0, SEEK_CUR);
                 if (pos != (off64_t)(-1)) {
-                    pos = lseek64(in_fd, pos - SECTOR_SIZE, SEEK_SET);
+                    pos = lseek64(in_fd, pos - bytes_read, SEEK_SET);
                     if (pos == (off64_t)(-1)) {
                         perror("Seek error");
                         error = 4;
@@ -267,41 +299,37 @@ int main(int argc, char * const argv[]) {
                     break;
                 }
             }
-            if (action == ACTION_ENCRYPT) {
-                if (crypt_cipher_encrypt(cipher, (char*)&sector, (char*)&out_sector, SECTOR_SIZE, (char*)&iv, sizeof(iv)) == 0) {
-                    if (write(out_fd, (char*)&out_sector, SECTOR_SIZE) != SECTOR_SIZE) {
-                        perror("Write error");
-                        error = 2;
+
+            for (size_t offset = 0; offset < (size_t)bytes_read; offset += SECTOR_SIZE)
+            {
+                *(unsigned int *)iv = scnt & 0xffffffff;
+
+                if (action == ACTION_ENCRYPT) {
+                    if (crypt_cipher_encrypt(cipher, read_buffer + offset,
+                                             write_buffer + offset, SECTOR_SIZE,
+                                             iv, sizeof(iv)) != 0) {
+                        fprintf(stderr, "Encryption error\n");
+                        error = 3;
                         break;
                     }
                 }
-                else {
-                    fprintf(stderr, "Encryption error\n");
-                    error = 3;
-                    break;
-                }
-            }
-            if (action == ACTION_DECRYPT) {
-                if (crypt_cipher_decrypt(cipher, (char*)&sector, (char*)&out_sector, SECTOR_SIZE, (char*)&iv, sizeof(iv)) == 0) {
-                    if (write(out_fd, (char*)&out_sector, SECTOR_SIZE) != SECTOR_SIZE) {
-                        perror("Write error");
-                        error = 2;
+                if (action == ACTION_DECRYPT) {
+                    if (crypt_cipher_decrypt(cipher, read_buffer + offset,
+                                             write_buffer + offset, SECTOR_SIZE,
+                                             iv, sizeof(iv)) != 0) {
+                        fprintf(stderr, "Decryption error\n");
+                        error = 3;
                         break;
                     }
                 }
-                else {
-                    fprintf(stderr, "Decryption error\n");
-                    error = 3;
+                if (write(out_fd, (char*)&write_buffer, bytes_read) != bytes_read) {
+                    perror("Write error");
+                    error = 2;
                     break;
                 }
-            }
-            scnt++;
-        }
-        if ((r > 0) && (r != SECTOR_SIZE)) {
-            fprintf(stderr, "Invalid input file size, should be multiple of %d bytes, output file truncated!\n", SECTOR_SIZE);
-            if (in_fd == out_fd) {
-                off64_t pos = lseek64(in_fd, 0, SEEK_CUR);
-                ftruncate(in_fd, pos - r);
+
+                scnt++;
+                offset += SECTOR_SIZE;
             }
         }
 
